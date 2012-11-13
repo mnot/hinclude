af19fd8d82ef92a811146d881fd96373e0787593
Mark Nottingham <mnot@mnot.net>
2012-11-13 16:01:07 +1100

    needs test in DIR

--- test/serve.sh
+++ test/serve.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp -f $DIR/../hinclude.js $DIR/assets/
-cd $DIR/assets; python -m SimpleHTTPServer 8080 2>/dev/null
+cp -f $DIR/hinclude.js $DIR/test/assets/
+cd $DIR/test/assets; python -m SimpleHTTPServer 8080 2>/dev/null
