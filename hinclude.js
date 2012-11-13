d39cdebeaead514b0ad056d6b3c8439528df3a23
Mark Nottingham <mnot@mnot.net>
2012-11-13 20:10:02 +1100

    make jslint happier

--- hinclude.js
+++ hinclude.js
@@ -26,118 +26,124 @@ SOFTWARE.
 See http://mnot.github.com/hinclude/ for documentation.
 */
 
+/*jslint indent: 2, browser: true, vars: true, nomen: true */
+/*global alert, ActiveXObject */
+
 var hinclude;
 
-(function() {
+(function () {
 
   "use strict";
 
   hinclude = {
     classprefix: "include_",
-    
+
     set_content_async: function (element, req) {
-      if (req.readyState == 4) {
-        if (req.status == 200 | req.status == 304) {
+      if (req.readyState === 4) {
+        if (req.status === 200 || req.status === 304) {
           element.innerHTML = req.responseText;
         }
-        element.className = hinclude['classprefix'] + req.status;
+        element.className = hinclude.classprefix + req.status;
       }
     },
 
-    buffer: new Array(),
+    buffer: [],
     set_content_buffered: function (element, req) {
-      if (req.readyState == 4) {
-        hinclude.buffer.push(new Array(element, req));
-        hinclude.outstanding--;
+      if (req.readyState === 4) {
+        hinclude.buffer.push([element, req]);
+        hinclude.outstanding -= 1;
         if (hinclude.outstanding === 0) {
           hinclude.show_buffered_content();
         }
       }
     },
-    
+
     show_buffered_content: function () {
       while (hinclude.buffer.length > 0) {
         var include = hinclude.buffer.pop();
-        if (include[1].status == 200 | include[1].status == 304) {
+        if (include[1].status === 200 || include[1].status === 304) {
           include[0].innerHTML = include[1].responseText;
         }
-        include[0].className = hinclude['classprefix'] + include[1].status;
+        include[0].className = hinclude.classprefix + include[1].status;
       }
     },
 
     outstanding: 0,
     includes: [],
     run: function () {
+      var i = 0;
       var mode = this.get_meta("include_mode", "buffered");
-      var callback = function(element, req) {};
+      var callback = function (element, req) {};
       this.includes = document.getElementsByTagName("hx:include");
       if (this.includes.length === 0) { // remove ns for IE
         this.includes = document.getElementsByTagName("include");
       }
-      if (mode == "async") {
+      if (mode === "async") {
         callback = this.set_content_async;
-      } else if (mode == "buffered") {
+      } else if (mode === "buffered") {
         callback = this.set_content_buffered;
         var timeout = this.get_meta("include_timeout", 2.5) * 1000;
         setTimeout(hinclude.show_buffered_content, timeout);
       }
-      for (var i=0; i < this.includes.length; i++) {
+      for (i; i < this.includes.length; i += 1) {
         this.include(this.includes[i], this.includes[i].getAttribute("src"), callback);
       }
     },
 
     include: function (element, url, incl_cb) {
-      var scheme = url.substring(0,url.indexOf(":"));
-      if (scheme.toLowerCase() == "data") { // just text/plain for now
-        var data = unescape(url.substring(url.indexOf(",") + 1, url.length));
+      var scheme = url.substring(0, url.indexOf(":"));
+      if (scheme.toLowerCase() === "data") { // just text/plain for now
+        var data = decodeURIComponent(url.substring(url.indexOf(",") + 1, url.length));
         element.innerHTML = data;
       } else {
         var req = false;
-        if(window.XMLHttpRequest) {
+        if (window.XMLHttpRequest) {
           try {
             req = new XMLHttpRequest();
-          } catch(e) {
+          } catch (e1) {
             req = false;
           }
         } else if (window.ActiveXObject) {
           try {
             req = new ActiveXObject("Microsoft.XMLHTTP");
-          } catch(e) {
+          } catch (e2) {
             req = false;
           }
         }
         if (req) {
-          this.outstanding++;
-          req.onreadystatechange = function() {
+          this.outstanding += 1;
+          req.onreadystatechange = function () {
             incl_cb(element, req);
           };
           try {
             req.open("GET", url, true);
             req.send("");
-          } catch (e) {
-            this.outstanding--;
-            alert("Include error: " + url + " (" + e + ")");
+          } catch (e3) {
+            this.outstanding -= 1;
+            alert("Include error: " + url + " (" + e3 + ")");
           }
         }
       }
     },
-    
-    refresh: function (element_id){
+
+    refresh: function (element_id) {
+      var i = 0;
       var mode = this.get_meta("include_mode", "buffered");
-      var callback = function(element, req) {};
+      var callback = function (element, req) {};
       callback = this.set_content_buffered;
-      for (var i=0; i < this.includes.length; i++) {
-        if(this.includes[i].getAttribute("id") == element_id){
+      for (i; i < this.includes.length; i += 1) {
+        if (this.includes[i].getAttribute("id") === element_id) {
           this.include(this.includes[i], this.includes[i].getAttribute("src"), callback);
         }
       }
     },
 
     get_meta: function (name, value_default) {
+      var m = 0;
       var metas = document.getElementsByTagName("meta");
-      for (var m=0; m < metas.length; m++) {
+      for (m; m < metas.length; m += 1) {
         var meta_name = metas[m].getAttribute("name");
-        if (meta_name == name) {
+        if (meta_name === name) {
           return metas[m].getAttribute("content");
         }
       }
@@ -154,48 +160,47 @@ var hinclude;
      *
      * Thrown together by Jesse Skinner (http://www.thefutureoftheweb.com/)
      */
-    addDOMLoadEvent: function(func) {
-      if (! window.__load_events) {
+    addDOMLoadEvent: function (func) {
+      if (!window.__load_events) {
         var init = function () {
+          var i = 0;
           // quit if this function has already been called
-          if (hinclude.addDOMLoadEvent.done) {return;}
+          if (hinclude.addDOMLoadEvent.done) {return; }
           hinclude.addDOMLoadEvent.done = true;
           if (window.__load_timer) {
             clearInterval(window.__load_timer);
             window.__load_timer = null;
           }
-          for (var i=0; i < window.__load_events.length; i++) {
+          for (i; i < window.__load_events.length; i += 1) {
             window.__load_events[i]();
           }
           window.__load_events = null;
           // clean up the __ie_onload event
-          /*@cc_on @*/
-          /*@if (@_win32)
-            document.getElementById("__ie_onload").onreadystatechange = "";
-          /*@end @*/
+          /*@cc_on
+          document.getElementById("__ie_onload").onreadystatechange = "";
+          @*/
         };
         // for Mozilla/Opera9
         if (document.addEventListener) {
           document.addEventListener("DOMContentLoaded", init, false);
         }
         // for Internet Explorer
-        /*@cc_on @*/
-        /*@if (@_win32)
-            document.write(
-              "<scr"
-              + "ipt id=__ie_onload defer src=javascript:void(0)><\/scr"
-              + "ipt>"
-            );
-            var script = document.getElementById("__ie_onload");
-            script.onreadystatechange = function() {
-                if (this.readyState == "complete") {
-                    init(); // call the onload handler
-                }
-            };
-        /*@end @*/
+        /*@cc_on
+        document.write(
+          "<scr"
+            + "ipt id=__ie_onload defer src=javascript:void(0)><\/scr"
+            + "ipt>"
+        );
+        var script = document.getElementById("__ie_onload");
+        script.onreadystatechange = function () {
+          if (this.readyState === "complete") {
+            init(); // call the onload handler
+          }
+        };
+        @*/
         // for Safari
         if (/WebKit/i.test(navigator.userAgent)) { // sniff
-          window.__load_timer = setInterval(function() {
+          window.__load_timer = setInterval(function () {
             if (/loaded|complete/.test(document.readyState)) {
               init();
             }
@@ -209,5 +214,5 @@ var hinclude;
     }
   };
 
-  hinclude.addDOMLoadEvent(function() {hinclude.run();});
+  hinclude.addDOMLoadEvent(function () {hinclude.run(); });
 }());
